687bb98d2fabab46cd8018ab7c0d6923cbae9a08
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2021-04-08 11:37:32 +0600

    navbar done

--- src/App.js
+++ src/App.js
@@ -10,31 +10,33 @@ import Home from './components/Home/Home';
 function App() {
 
   return (
-    <Router>
-        <Switch>
+    <div className='container-style'>
+      <Router>
+          <Switch>
 
-          <Route exact path="/">
-            <Home />
-          </Route>
+            <Route exact path="/">
+              <Home />
+            </Route>
 
-          <Route path="/home">
-            <Home />
-          </Route>
+            <Route path="/home">
+              <Home />
+            </Route>
 
-          <Route path="/Cart">
-            <Home />
-          </Route>
+            <Route path="/cart">
+              <Home />
+            </Route>
 
-          <Route path="/Login">
-            <Home />
-          </Route>
+            <Route path="/login">
+              <Home />
+            </Route>
 
-          <Route path="/SignUp">
-            <Home />
-          </Route>
+            <Route path="/signUp">
+              <Home />
+            </Route>
 
-        </Switch>
-    </Router>
+          </Switch>
+      </Router>
+    </div>
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand> <Link to='/' className='navbar-brand'>React-Bootstrap</Link> </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="ml-auto">
-                        <Nav.Link href="#deets">More deets</Nav.Link>
-                        <Nav.Link eventKey={2} href="#memes">
-                            Dank memes
-                        </Nav.Link>
+                        <Link to='/' className='nav-link mr-3'>Home</Link>
+                        <Link to='/cart' className='nav-link mr-3'>Cart</Link>
+                        <Link to='/login' className='nav-link mr-3'>Login</Link>
+                        <Link to='/signUp' className='nav-link mr-3'>SignUp</Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
