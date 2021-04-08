f2408ddb44e10b88fc1477ace9e8363dadbc8289
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2021-04-08 13:15:00 +0600

    header done

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useForm } from "react-hook-form";
 import bg from "../../images/bannerbackground.png"
+import './SearchBar.css'
 
 const SearchBar = () => {
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
@@ -18,7 +19,7 @@ const SearchBar = () => {
 
             <form onSubmit={handleSubmit(onSubmit)} >
                 <div className='d-flex justify-content-center align-items-center'>
-                    <input {...register("name", { required: true })} className='p-1' />
+                    <input {...register("name", { required: true })} className='input-st' />
                     <input type="submit" className='btn btn-success ml-2'/>
                 </div>
                 {errors.name && <span className='d-block text-danger'>Food Name is required</span>}
